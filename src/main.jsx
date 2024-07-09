e828c9d3cd673ae277e783e34063686937b172a2
Sophia <lsofa1204@gmail.com>
2024-07-09 22:41:04 +0300

    fix architecture

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/reset.css';
+import './styles/styles.css';
+import { Main } from './components/Main.jsx';
+import { Header } from './components/Header.jsx';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <React.StrictMode>
+        <>
+            <Header/>
+            <Main />
+        </>
+    </React.StrictMode>,
+)
